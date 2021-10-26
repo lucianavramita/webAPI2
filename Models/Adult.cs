29c25543c7e645bd189e0192cbfe3fdbc868c763
Lucian Avramita <lucian.avramita@gmail.com>
2021-10-26 13:40:06 -0700

    Initial Commit

---
+++ Models/Adult.cs
@@ -0,0 +1,14 @@
+namespace webAPI2.Models
+{
+    public class Adult
+    {
+     public int Id { get; set; } = 0;
+     public string FirstName { get; set; } = "";
+     public string LastName { get; set; } = "";
+     public string HairColor { get; set; } = "";
+     public string EyeColor { get; set; } = "";
+     public int Age { get; set; } = 0;
+     public float Weight { get; set; } = 0;
+    public char Sex { get; set; } = 'N';
+    }
+}
